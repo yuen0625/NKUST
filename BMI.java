477656fe9b53381da082cd16b4c97e62a4849d27
yuen0625 <jusy58248@gmail.com>
2021-11-16 14:11:53 +0800

    add.BMI

---
+++ BMI.java
@@ -0,0 +1,9 @@
+public class BMI {
+    public static void main(String[] args) {
+        float h=1.54f;
+        int w = 40;
+        float sum = 0;
+        sum=w/(h*h);
+        System.out.println(sum);
+    }
+}
