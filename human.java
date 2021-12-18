28f75d65fbe7b2c1e7aa5eab9c45a3aa1522e572
yuen0625 <93572403+yuen0625@users.noreply.github.com>
2021-12-18 23:41:48 +0800

    Add files via upload

---
+++ human.java
@@ -0,0 +1,21 @@
+import com.sun.tools.javac.Main;
+public class human {
+    String name;
+    double hp;
+    double att;
+    double mp;
+    String sname1;//技能1
+    String sname2;//技能2
+    String sname3;//技能3
+    String describe_skill1;//技能1描述
+    String describe_skill2;//技能2描述
+    String describe_skill3;//技能3描述
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
